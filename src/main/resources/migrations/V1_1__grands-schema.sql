5e3ecb173b4bff3b98dab2d5bf28c5f08b97213e
bxtsang <bxtsang.2018@sis.smu.edu.sg>
2021-08-02 14:01:36 +0800

    Create flyway db migrations

---
+++ src/main/resources/migrations/V1_1__grands-schema.sql
@@ -0,0 +1,21 @@
+drop table if exists person;
+drop table if exists household;
+
+create table household (
+    id int not null primary key,
+    housing_type varchar(11) not null
+);
+
+create table person (
+    id int not null primary key,
+    name varchar(30) not null,
+    gender char(1) not null,
+    marital_status varchar(7) not null,
+    spouse int,
+    occupation_type varchar(10) not null,
+    annual_income int not null,
+    dob date not null,
+    household_id int not null,
+    foreign key (spouse) references person(id),
+    foreign key (household_id) references household(id)
+);
